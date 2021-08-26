6cdb47c2d33f66b5e44f91f48213c002db52a903
Clark Winkelmann <clark.winkelmann@gmail.com>
2021-08-27 00:52:21 +0200

    Show price range in product list

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -5,6 +5,8 @@ import {forum} from './compat';
 import Select from 'flarum/common/components/Select';
 import Product from 'flamarkt/core/common/models/Product';
 import ProductShowLayout from 'flamarkt/core/forum/layouts/ProductShowLayout';
+import ProductListItem from 'flamarkt/core/forum/components/ProductListItem';
+import PriceLabel from 'flamarkt/core/common/components/PriceLabel';
 import Model from 'flarum/common/Model';
 
 export {
@@ -63,5 +65,55 @@ app.initializers.add('flamarkt-variants', () => {
                 this.selectedVariantIndex = variants.findIndex(variant => variant.id() === id);
             },
         }), 60);
+    });
+
+    extend(ProductListItem.prototype, 'items', function (this: ProductListItem, items: ItemList) {
+        const {product} = this.attrs;
+
+        if (!product.attribute('isVariantMaster')) {
+            return;
+        }
+
+        const variants = product.variants();
+
+        if (!Array.isArray(variants) || variants.length < 2) {
+            return;
+        }
+
+        let minPrice: number | null = null;
+        let maxPrice: number | null = null;
+
+        variants.forEach(variant => {
+            const value = variant.price();
+
+            if (!value) {
+                return;
+            }
+
+            if (minPrice === null || value < minPrice) {
+                minPrice = value;
+            }
+
+            if (maxPrice === null || value > maxPrice) {
+                maxPrice = value;
+            }
+        });
+
+        // If there wasn't a single valid price, minPrice will be null
+        // If the min anx max prices are same, no need to do anything
+        // TODO: still if the price is the same, the parent might not have any price
+        if (minPrice === null || minPrice === maxPrice) {
+            return;
+        }
+
+        items.replace('price', m('span.ProductListItem--price', [
+            m(PriceLabel, {
+                value: minPrice,
+            }),
+            ' - ',
+            m(PriceLabel, {
+                value: maxPrice,
+            }),
+        ]))
     });
 });
